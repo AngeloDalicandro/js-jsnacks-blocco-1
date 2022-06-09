7c09731917d87ad6659963fce8b2a7a21fa3b25d
AngeloDalicandro <angelo.dalicandro1@gmail.com>
2022-06-09 14:19:50 +0200

    completed

---
+++ jsnack4/js/script.js
@@ -0,0 +1,16 @@
+// Creo un array vuoto
+const myArray = [];
+
+// Imposto il loop per richiedere i numeri all'utente 6 volte
+for( let i = 0; i < 6; i++) {
+    // Chiedo il numero tramite un promp
+    let userNumber = prompt('Dammi un numero');
+
+    // Imposto la condizione per stamparlo nell'array e stampo
+    if( userNumber % 2 !== 0 ) {
+        myArray.push( userNumber );
+    }
+}
+
+// Stampo in console l'array per verificare che l'esercizio sia corretto
+console.log(myArray)
